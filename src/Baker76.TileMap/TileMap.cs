e7eb3a749f14a3ca3434b9001cf88617b238baee
benbaker76 <headkaze@gmail.com>
2025-07-26 23:19:15 -0700

    Add CreateTiledTmx

--- src/Baker76.TileMap/TileMap.cs
+++ src/Baker76.TileMap/TileMap.cs
@@ -30,13 +30,13 @@ namespace Baker76.TileMap
 
     public class TileMapSliceOptions
     {
-        public int TileWidth = 0;
-        public int TileHeight = 0;
-        public int TileSetWidth = 0;
+        public int TileWidth = 8;
+        public int TileHeight = 8;
+        public int TileSetWidth = 256;
         public bool NoRepeat = true;
-        public bool NoMirror = true;
+        public bool NoMirror = false;
         public bool NoRotate = false;
-        public bool InsertBlankTile = true;
+        public bool InsertBlankTile = false;
         public int ClearMap = -1;
         public TileLayerAttributes TileLayerAttributes = TileLayerAttributes.Extended512;
     }
@@ -707,6 +707,151 @@ namespace Baker76.TileMap
             TileMap tileMap = new TileMap(name, (ushort)tileCols, (ushort)tileRows, (byte)options.TileWidth, (byte)options.TileHeight, options.TileLayerAttributes, tileData, name);
 
             return (tileMap, destImage);
+        }
+
+        public static void CreateTiledTmx(string srcImagePath, TileMapSliceOptions options)
+        {
+            string srcImagePathWithoutExtension = Path.GetFileNameWithoutExtension(srcImagePath);
+            string dstTmxPath = srcImagePathWithoutExtension + ".tmx";
+            string dstImagePath = srcImagePathWithoutExtension + "Tiles.png";
+            Image srcImage = PngReader.Read(srcImagePath);
+            if (srcImage == null)
+            {
+                Console.WriteLine("ERROR: Could not read the PNG file.");
+                return;
+            }
+            if (srcImage.Width % options.TileWidth != 0 || srcImage.Height % options.TileHeight != 0)
+            {
+                Console.WriteLine("ERROR: The image dimensions are not multiples of the tile size.");
+                return;
+            }
+            int tileCols = srcImage.Width / options.TileWidth;
+            int tileRows = srcImage.Height / options.TileHeight;
+            List<TiledNode> tiledNodes = new List<TiledNode>();
+            List<Image> uniqueTiles = new List<Image>();
+            Dictionary<ulong, TiledNode> tileDictionary = new Dictionary<ulong, TiledNode>();
+            int tileCount = 0;
+            int bitsPerPixel = ((srcImage.BitsPerPixel <= 8) ? 8 : 32);
+            if (options.InsertBlankTile)
+            {
+                Image blankTile = new Image(options.TileWidth, options.TileHeight, bitsPerPixel, srcImage.Palette);
+                uniqueTiles.Add(blankTile);
+                tileDictionary[blankTile.GetHash()] = new TiledNode(tileCount++, TiledAttributes.None);
+            }
+            for (int y = 0; y < tileRows; y++)
+            {
+                for (int x = 0; x < tileCols; x++)
+                {
+                    Rectangle srcRect = new Rectangle(x * options.TileWidth, y * options.TileHeight, options.TileWidth, options.TileHeight);
+                    Image tileImage = new Image(options.TileWidth, options.TileHeight, bitsPerPixel, srcImage.Palette);
+                    tileImage.DrawImage(srcImage, new Rectangle(0, 0, tileImage.Width, tileImage.Height), srcRect);
+                    ulong hash = tileImage.GetHash();
+                    if (!tileDictionary.ContainsKey(hash))
+                    {
+                        int id = tileCount++;
+                        if (options.NoRepeat)
+                        {
+                            tileDictionary.Add(hash, new TiledNode(id, TiledAttributes.None));
+                        }
+                        if (options.NoMirror)
+                        {
+                            ulong noMirrorHash = tileImage.GetHash(ImageAttributes.MirrorX);
+                            if (!tileDictionary.ContainsKey(noMirrorHash))
+                            {
+                                tileDictionary.Add(noMirrorHash, new TiledNode(id, TiledAttributes.Horizontal));
+                            }
+                            noMirrorHash = tileImage.GetHash(ImageAttributes.MirrorY);
+                            if (!tileDictionary.ContainsKey(noMirrorHash))
+                            {
+                                tileDictionary.Add(noMirrorHash, new TiledNode(id, TiledAttributes.Vertical));
+                            }
+                            noMirrorHash = tileImage.GetHash(ImageAttributes.MirrorX_Y);
+                            if (!tileDictionary.ContainsKey(noMirrorHash))
+                            {
+                                tileDictionary.Add(noMirrorHash, new TiledNode(id, TiledAttributes.Horizontal_Vertical));
+                            }
+                        }
+                        if (options.NoRotate)
+                        {
+                            ulong noRotateHash = tileImage.GetHash(ImageAttributes.MirrorX_Y | ImageAttributes.Rotate);
+                            if (!tileDictionary.ContainsKey(noRotateHash))
+                            {
+                                tileDictionary.Add(noRotateHash, new TiledNode(id, TiledAttributes.AntiDiagonal | TiledAttributes.Vertical));
+                            }
+                            noRotateHash = tileImage.GetHash(ImageAttributes.Rotate | ImageAttributes.MirrorX);
+                            if (!tileDictionary.ContainsKey(noRotateHash))
+                            {
+                                tileDictionary.Add(noRotateHash, new TiledNode(id, TiledAttributes.Horizontal_Vertical | TiledAttributes.AntiDiagonal));
+                            }
+                            noRotateHash = tileImage.GetHash(ImageAttributes.Rotate | ImageAttributes.MirrorY);
+                            if (!tileDictionary.ContainsKey(noRotateHash))
+                            {
+                                tileDictionary.Add(noRotateHash, new TiledNode(id, TiledAttributes.AntiDiagonal));
+                            }
+                            noRotateHash = tileImage.GetHash(ImageAttributes.Rotate);
+                            if (!tileDictionary.ContainsKey(noRotateHash))
+                            {
+                                tileDictionary.Add(noRotateHash, new TiledNode(id, TiledAttributes.AntiDiagonal | TiledAttributes.Horizontal));
+                            }
+                        }
+                        uniqueTiles.Add(tileImage);
+                    }
+                    tiledNodes.Add(tileDictionary[hash]);
+                }
+            }
+            int tileSetWidth = options.TileSetWidth;
+            if (tileSetWidth == 0)
+            {
+                tileSetWidth = Baker76.Imaging.Utility.CalculateTextureSize(options.TileWidth, options.TileHeight, uniqueTiles.Count);
+            }
+            int destRows = (uniqueTiles.Count + tileSetWidth / options.TileWidth - 1) / (tileSetWidth / options.TileWidth);
+            int destCols = tileSetWidth / options.TileWidth;
+            Image pngDst = new Image(tileSetWidth, destRows * options.TileHeight, bitsPerPixel, srcImage.Palette);
+            int tileIndex = 0;
+            for (int destRow = 0; destRow < destRows; destRow++)
+            {
+                for (int destCol = 0; destCol < destCols; destCol++)
+                {
+                    if (tileIndex >= uniqueTiles.Count)
+                    {
+                        break;
+                    }
+                    Image tile = uniqueTiles[tileIndex];
+                    int destX = destCol * options.TileWidth;
+                    int destY = destRow * options.TileHeight;
+                    Rectangle destRect = new Rectangle(destX, destY, tile.Width, tile.Height);
+                    pngDst.DrawImage(tile, destRect, new Rectangle(0, 0, tile.Width, tile.Height));
+                    tileIndex++;
+                }
+            }
+            PngWriter.Write(dstImagePath, pngDst);
+            using StreamWriter writer = new StreamWriter(dstTmxPath);
+            writer.WriteLine("<?xml version=\"1.0\" encoding=\"UTF-8\"?>");
+            writer.WriteLine("<map version=\"1.10\" tiledversion=\"1.10.2\" orientation=\"orthogonal\" renderorder=\"right-down\" width=\"" + tileCols + "\" height=\"" + tileRows + "\" tilewidth=\"" + options.TileWidth + "\" tileheight=\"" + options.TileHeight + "\" infinite=\"0\" nextlayerid=\"2\" nextobjectid=\"1\">");
+            writer.WriteLine(" <tileset firstgid=\"1\" name=\"tiles\" tilewidth=\"" + options.TileWidth + "\" tileheight=\"" + options.TileHeight + "\" tilecount=\"" + uniqueTiles.Count + "\" columns=\"" + destCols + "\">");
+            writer.WriteLine("  <image source=\"" + Path.GetFileName(dstImagePath) + "\" width=\"" + pngDst.Width + "\" height=\"" + pngDst.Height + "\"/>");
+            writer.WriteLine(" </tileset>");
+            writer.WriteLine(" <layer id=\"1\" name=\"Tile Layer 1\" width=\"" + tileCols + "\" height=\"" + tileRows + "\">");
+            writer.WriteLine("  <data encoding=\"csv\">");
+            for (int i = 0; i < tiledNodes.Count; i++)
+            {
+                writer.Write((options.ClearMap == -1) ? tiledNodes[i].ToLong() : options.ClearMap);
+                if (i < tiledNodes.Count - 1)
+                {
+                    writer.Write(",");
+                }
+                else
+                {
+                    writer.WriteLine();
+                }
+                if (i % tileCols == tileCols - 1)
+                {
+                    writer.WriteLine();
+                }
+            }
+            writer.WriteLine("  </data>");
+            writer.WriteLine(" </layer>");
+            writer.WriteLine("</map>");
         }
 
         public static TileAttributes TiledToTileAttributes(TiledAttributes tiledAttributes)
